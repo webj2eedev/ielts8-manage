0293e63d6860158d05584f54bc7b915eed63c7e8
webj2ee <webj2ee@qq.com>
2022-12-03 17:20:34 +0800

    不清楚为什么...

--- src/main/java/com/webj2eedev/ieltsnote/dto/WordDeleteDTO.java
+++ src/main/java/com/webj2eedev/ieltsnote/dto/WordDeleteDTO.java
@@ -1,12 +1,16 @@
 package com.webj2eedev.ieltsnote.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Builder
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class WordDeleteDTO implements Serializable {
-    private Integer uid;
+    private int uid;
 }
