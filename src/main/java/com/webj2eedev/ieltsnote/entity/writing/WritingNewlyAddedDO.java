a0c6695ae41326a6dd979ca79057e8cd2e6e8f01
webj2ee <webj2ee@qq.com>
2023-01-11 17:16:29 +0800

    Chart-Writing

--- src/main/java/com/webj2eedev/ieltsnote/entity/writing/WritingNewlyAddedDO.java
+++ src/main/java/com/webj2eedev/ieltsnote/entity/writing/WritingNewlyAddedDO.java
@@ -8,7 +8,7 @@ import java.math.BigDecimal;
 @Builder
 @Data
 public class WritingNewlyAddedDO {
+    private String time;
     private BigDecimal task1;
     private BigDecimal task2;
-    private String time;
 }
